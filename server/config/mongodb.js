570c4e73f7a039c8070047f488ac4f82b97683dd
HamzahM03 <138250911+HamzahM03@users.noreply.github.com>
2025-02-20 23:00:00 -0500

    Update mongodb.js
    Fix connection so that we connect to evo-flexDB not testDB which is defaulted by mongoAtlas.

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -6,7 +6,8 @@ const connectDB = async() => {
     console.log("Database Connected")
   );
 
-  await mongoose.connect(`${process.env.MONGODB_URL}/evo-flex`);
+  await mongoose.connect(process.env.MONGODB_URL, { dbName: "evo-flex" });
+
 };
 
 export default connectDB;

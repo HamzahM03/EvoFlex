3203779107f1ca26ce63be485b9ac66a97cab283
HamzahM03 <138250911+HamzahM03@users.noreply.github.com>
2025-02-20 19:17:14 -0500

    Create authRouter
    Add authentication routes with Express router, including login, logout, and registration. Integrate routes under '/api/auth'.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,9 +14,8 @@ app.use(cors({credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
-
-app.get("/api", (req,res) => {
-  res.json({"users": ["userOne", "userTwo", "userThree"]});
-});
+// API Endpoints
+app.get("/", (req,res)=>res.send("API Working"));
+app.use('/api/auth', authRouter);
 
 app.listen(port, () => {console.log(`Server started on port ${port}`)});
